4869a09f085642abb59b73967a7977ac6328422f
Iasmim Siqueira <iasmimsas@gmail.com>
2025-08-20 10:48:27 -0300

    not found exception

--- backend/instagram/src/main/java/br/edu/ifpb/instagram/exception/RestExceptionHandler.java
+++ backend/instagram/src/main/java/br/edu/ifpb/instagram/exception/RestExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Map;
@@ -21,5 +22,18 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
         // Retorna a resposta com o status HTTP 409 Conflict
         return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex) {
+        // Você pode retornar a mensagem de erro da exceção ou uma mensagem personalizada
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+    
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
